ec1087007d1eed13ba42ba555b15a536ebca121b
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-11 17:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("click",click);
+
+
+function click(){
+	let count=document.getElementById('counter');
+	let num=count.innerHTML;
+	alert(num);
+	num++;
+	counter.innerHTML=num;
+}
