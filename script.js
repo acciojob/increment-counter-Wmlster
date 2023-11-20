be3b7fd239308daad9a40b1aa81d833e206991c4
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-20 18:46:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //your JS code here. If required.
-document.addEventListener("click",click); 
 
-let Counter=0;
-
-function click(){
+function incrementBtn(){  
     Counter=document.getElementById('counter').innerHTML;
 	alert(Counter);
 	Counter++;
 	document.getElementById('counter').innerHTML=Counter;
 }
+
